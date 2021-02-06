aa8eb6f29d7bc2c8713684d0f6ebcf23177fe7cc
Krzysztof Mazur <m.krzysztof@hotmail.com>
2021-02-06 15:42:22 +0100

    Moved load/create to new Class

---
+++ SharpWallet/CreateLoadConfig.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace SharpWallet
+{
+    class CreateLoadConfig
+    {
+        private string configFilename;
+        public int windowWidth { get; set; }
+        public int windowHeight { get; set; }
+        public int windowPositionX { get; set; }
+        public int windowPositionY { get; set; }
+
+        private string databaseFileName;
+
+        public CreateLoadConfig()
+        {
+            configFilename = "sharpwallet.xml";
+        }
+
+        public void setWindowSize(int width, int height)
+        {
+            this.windowWidth = width;
+            this.windowHeight = height;
+        }
+
+        public void setWindowPosition(int x, int y)
+        {
+            this.windowPositionX = x;
+            this.windowPositionY = y;
+        }
+
+        // create the xml file
+        public void CreateConfigFile()
+        {
+            using (XmlWriter writer = XmlWriter.Create(configFilename))
+            {
+                writer.WriteStartElement("database");
+                writer.WriteElementString("windowHeight", Convert.ToString(windowHeight));
+                writer.WriteElementString("windowWidth", Convert.ToString(windowWidth));
+                writer.WriteElementString("windowPositionX", Convert.ToString(windowPositionX));
+                writer.WriteElementString("windowPositionY", Convert.ToString(windowPositionY));
+                writer.WriteElementString("databaseFileName", databaseFileName);
+                writer.WriteEndElement();
+
+                writer.Flush();
+            }
+        }
+        // try to load xml
+        public void LoadConfig()
+        {
+            XmlTextReader reader = null;
+
+            try
+            {
+                reader = new XmlTextReader(configFilename);
+                string element;
+                bool windowHeight = false;
+                bool windowWidth = false;
+                bool windowPositionX = false;
+                bool windowPositionY = false;
+                bool databaseFileName = false;
+
+
+                while (reader.Read())
+                {
+                    switch (reader.NodeType)
+                    {
+                        case XmlNodeType.Element:
+                            element = reader.Name;
+                            if (element == "windowHeight")
+                            {
+                                windowHeight = true;
+                            }
+                            else if (element == "windowWidth")
+                            {
+                                windowWidth = true;
+                            }
+                            else if (element == "windowPositionX")
+                            {
+                                windowPositionX = true;
+                            }
+                            else if (element == "windowPositionY")
+                            {
+                                windowPositionY = true;
+                            }
+                            else if (element == "databaseFileName")
+                            {
+                                databaseFileName = true;
+                            }
+                            break;
+
+                        case XmlNodeType.Text:
+                            if (windowHeight)
+                            {
+                                windowHeight = false;
+                                this.windowHeight = Convert.ToInt32(reader.Value);
+                            }
+                            else if (windowWidth)
+                            {
+                                windowWidth = false;
+                                this.windowWidth = Convert.ToInt32(reader.Value);
+                            }
+                            else if (windowPositionX)
+                            {
+                                windowPositionX = false;
+                                this.windowPositionX = Convert.ToInt32(reader.Value);
+                            }
+                            else if (windowPositionY)
+                            {
+                                windowPositionY = false;
+                                this.windowPositionY = Convert.ToInt32(reader.Value);
+                            }
+                            else if (databaseFileName)
+                            {
+                                databaseFileName = false;
+                                this.databaseFileName = reader.Value;
+                            }
+                            break;
+                    }
+                }
+
+            }
+            finally
+            {
+                if (reader != null)
+                {
+                    reader.Close();
+                }
+            }
+        }
+    }
+}
--- SharpWallet/Form1.cs
+++ SharpWallet/Form1.cs
@@ -14,150 +14,40 @@ namespace SharpWallet
 {
     public partial class MainFrm : Form
     {
-        private string configFilename;
-        private int windowWidth, windowHeight;
-        private int windowPositionX, windowPositionY;
-        private string databaseFileName;
-
+        CreateLoadConfig configFile;
         public MainFrm()
         {
             InitializeComponent();
+            configFile = new CreateLoadConfig();
 
-            configFilename = "sharpwallet.xml";
+            string configFilename = "sharpwallet.xml";
 
             if(!File.Exists(configFilename))
             {
-                this.windowHeight = this.Height;
-                this.windowWidth = this.Width;
+                configFile.setWindowSize(this.Width, this.Height);
                 
-                windowPositionX = Screen.PrimaryScreen.Bounds.Width / 2 - this.windowWidth / 2;
-                windowPositionY = Screen.PrimaryScreen.Bounds.Height / 2 - this.windowHeight / 2;
+                int windowPositionX = Screen.PrimaryScreen.Bounds.Width / 2 - this.Width / 2;
+                int windowPositionY = Screen.PrimaryScreen.Bounds.Height / 2 - this.Height / 2;
 
-                databaseFileName = Directory.GetCurrentDirectory() + "\\sharpwallet.json";
-                CreateConfigFile();
+                configFile.setWindowPosition(windowPositionX, windowPositionY);
+                configFile.CreateConfigFile();
             }
 
-            LoadConfig();
-        }
-
-        // try to load xml
-        private void LoadConfig()
-        {
-            XmlTextReader reader = null;
-
-            try
-            {
-                reader = new XmlTextReader(configFilename);
-                string element;
-                bool windowHeight = false;
-                bool windowWidth = false;
-                bool windowPositionX = false;
-                bool windowPositionY = false;
-                bool databaseFileName = false;
-
-
-                while (reader.Read())
-                {
-                    switch(reader.NodeType)
-                    {
-                        case XmlNodeType.Element:
-                            element = reader.Name;
-                            if (element == "windowHeight")
-                            {
-                                windowHeight = true;
-                            }
-                            else if(element == "windowWidth")
-                            {
-                                windowWidth = true;
-                            }
-                            else if(element == "windowPositionX")
-                            {
-                                windowPositionX = true;
-                            }
-                            else if(element == "windowPositionY")
-                            {
-                                windowPositionY = true;
-                            }
-                            else if(element == "databaseFileName")
-                            {
-                                databaseFileName = true;
-                            }
-                            break;
-
-                        case XmlNodeType.Text:
-                            if (windowHeight)
-                            {
-                                windowHeight = false;
-                                this.windowHeight = Convert.ToInt32(reader.Value);
-                            }
-                            else if (windowWidth)
-                            {
-                                windowWidth = false;
-                                this.windowWidth = Convert.ToInt32(reader.Value);
-                            }
-                            else if (windowPositionX)
-                            {
-                                windowPositionX = false;
-                                this.windowPositionX = Convert.ToInt32(reader.Value);
-                            }
-                            else if (windowPositionY)
-                            {
-                                windowPositionY = false;
-                                this.windowPositionY = Convert.ToInt32(reader.Value);
-                            }
-                            else if (databaseFileName)
-                            {
-                                databaseFileName = false;
-                                this.databaseFileName = reader.Value;
-                            }
-                            break;
-                    }
-                }
-
-            } 
-            finally
-            {
-                if(reader != null)
-                {
-                    reader.Close();
-                }
-            }
-
+            configFile.LoadConfig();
         }
 
         private void MainFrm_FormClosed(object sender, FormClosedEventArgs e)
         {
-            this.windowHeight = this.Height;
-            this.windowWidth = this.Width;
-            this.windowPositionX = this.Left;
-            this.windowPositionY = this.Top;
-
-            CreateConfigFile();
-        }
-
-        // create the xml file
-        private void CreateConfigFile()
-        {
-            using(XmlWriter writer = XmlWriter.Create(configFilename))
-            {
-                writer.WriteStartElement("database");
-                writer.WriteElementString("windowHeight", Convert.ToString(windowHeight));
-                writer.WriteElementString("windowWidth", Convert.ToString(windowWidth));
-                writer.WriteElementString("windowPositionX", Convert.ToString(windowPositionX));
-                writer.WriteElementString("windowPositionY", Convert.ToString(windowPositionY));
-                writer.WriteElementString("databaseFileName", databaseFileName);
-                writer.WriteEndElement();
-
-                writer.Flush();
-            }
+            configFile.setWindowSize(this.Width, this.Height);
+            configFile.setWindowPosition(this.Left, this.Top);
+            configFile.CreateConfigFile();
         }
 
         private void MainFrm_Load(object sender, EventArgs e)
         {
-            this.Location = new Point(windowPositionX, windowPositionY);
-            this.Width = windowWidth;
-            this.Height = windowHeight;
-
+            this.Location = new Point(configFile.windowPositionX, configFile.windowPositionY);
+            this.Width = configFile.windowWidth;
+            this.Height = configFile.windowHeight;
         }
     }
 }
